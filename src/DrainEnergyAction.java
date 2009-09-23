3f7f7bc187c5179729b9a15b173016b828f78cf0
U-LAPTOP-WORK\tfinnie <tfinnie@laptop-work.(none)>
2009-09-23 19:46:20 +0800

    simplified slightly

--- src/DrainEnergyAction.java
+++ src/DrainEnergyAction.java
@@ -3,7 +3,10 @@ import lejos.util.TimerListener;
 
 public class DrainEnergyAction implements Action {
 
+    // how much to decrease the energy level by each time
     private static final double DECREASE_AMOUNT = 0.05;
+
+    // how long to wait between each decrease
     private static final int TIMER_INTERVAL_MILLIS = 1000;
 
     private EnergyLevel energyLevel;
@@ -16,7 +19,7 @@ public class DrainEnergyAction implements Action {
         this.energyLevel = energyLevel;
 
         // Create the timer that will fire every TIMER_INTERVAL_MILLS
-        timer = new Timer(TIMER_INTERVAL_MILLIS, new DecreaseEnergy(DECREASE_AMOUNT));
+        timer = new Timer(TIMER_INTERVAL_MILLIS, new DecreaseEnergy());
     }
 
     // Action methods
@@ -24,22 +27,15 @@ public class DrainEnergyAction implements Action {
         timer.start();
     }
 
-    // TODO: add suppress to Action interface
     public void suppress() {
         timer.stop();
     }
 
     private class DecreaseEnergy implements TimerListener {
 
-        private double amount;
-
-        public DecreaseEnergy(double amount) {
-            this.amount = amount;
-        }
-
         public void timedOut() {
             double energy = energyLevel.getEnergyLevel();
-            energy-=amount;
+            energy-=DECREASE_AMOUNT;
             if (energy > 0) {
                 energyLevel.setEnergyLevel(energy);
             }
