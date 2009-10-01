f96672cf5e7bf44b768a4edd0a32b7376377258a
mal <mal@ubuntu.(none)>
2009-10-01 12:30:04 +0800

    Avoids wall if it touches it

--- src/AvoidObstaclesBehavior.java
+++ src/AvoidObstaclesBehavior.java
@@ -1,20 +1,23 @@
 import lejos.subsumption.Behavior;
 import lejos.nxt.UltrasonicSensor;
 import lejos.navigation.TachoPilot;
+import lejos.nxt.TouchSensor;
 
 public class AvoidObstaclesBehavior implements Behavior {
 
   private UltrasonicSensor sonic;
   private TachoPilot pilot;
+  private TouchSensor touch;
   private static final int DISTANCE_THRESHOLD = 10;
 
-  public AvoidObstaclesBehavior(UltrasonicSensor sonic, TachoPilot pilot) {
+  public AvoidObstaclesBehavior(UltrasonicSensor sonic, TouchSensor touch, TachoPilot pilot) {
     this.sonic = sonic;
     this.pilot = pilot;
+    this.touch = touch;
   }
 
   public boolean takeControl() {
-    return sonic.getDistance() <= DISTANCE_THRESHOLD;
+    return (sonic.getDistance() <= DISTANCE_THRESHOLD || touch.isPressed());
   }
 
   public void suppress() {
